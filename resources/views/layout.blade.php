5dd83a77cf231cfba2a07b216fb2564153299da7
olszewskik <kamil.web@gmail.com>
2022-06-24 23:38:05 +0200

    init project

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>JehuApp</title>
+    <link rel="stylesheet" href={{ asset('css/app.css') }}>
 </head>
 <body>
+    <div class="container">
     <menu>
         <li><a href='/register'>Register</a></li>
         <li><a href='/settings/users'>Users</a></li>
@@ -14,7 +16,9 @@
         <li><a href='/settings/places'>Places</a></li>
         <li><a href='/settings/trolleys'>Trolleys</a></li>
     </menu>
+    </div>
     
     @yield('content')
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
