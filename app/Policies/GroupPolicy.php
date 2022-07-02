0276c3e6eb7836fdebb2a0d1b422515b6d41b202
olszewskik <kamil.web@gmail.com>
2022-07-02 18:23:45 +0200

    refactor

--- app/Policies/GroupPolicy.php
+++ app/Policies/GroupPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Group;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
@@ -10,11 +11,83 @@ class GroupPolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view any models.
      *
-     * @return void
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function __construct()
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Group  $group
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Group $group)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Group  $group
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Group $group)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Group  $group
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Group $group)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Group  $group
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Group $group)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Group  $group
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Group $group)
     {
         //
     }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,40 +8,55 @@
     <link rel="stylesheet" href={{ asset('css/app.css') }}>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
+        <a class="navbar-brand" href="/">Home</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href='/manage/users'>Users</a>
+              <a class="nav-link" href={{ route('users') }}>Users</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href={{ route('group.index') }}>Groups</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href={{ route('places.index') }}>Places</a>
-            </li>
+            </li>  
             <li class="nav-item">
-              <a class="nav-link" href='/manage/trolleys'>Trolleys</a>
-            </li>
+              <a class="nav-link" href={{ route('general.edit') }}>Settings</a>
+            </li>  
           </ul>
           <div class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-              @auth
-              <span>Welcome {{auth()->user()->name}}</span>
-              <form method="POST" action="/logout">
+            @auth
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <span class="nav-link">Welcome {{auth()->user()->name}}</span>
+              </li>
+              <li class="nav-item">
+                <form class="d-flex" method="POST" action="/logout">
                   @csrf
-                  <button type="submit">
-                      logout
+                  <button class="btn btn-outline-light" type="submit">
+                    logout
                   </button>
-              </form>
-              @else
+                </form>
+              </li>
+            </ul>
+            @else
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
               <a class="nav-link" href='/register'>Register</a>
+            </li>
+            <li class="nav-item">
               <a class="nav-link" href='/login'>Login</a>
-              @endauth
-          </div>
+            </li>
+            </ul>
+            @endauth
+        </div>
+
+
         </div>
       </div>
     </nav>
