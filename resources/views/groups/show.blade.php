2ba35ddba9e7783f672b8dc333350e037d47cd8a
olszewskik <kamil.web@gmail.com>
2022-07-09 19:06:29 +0200

    modal component refactor

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -2,30 +2,8 @@
 
 @section('content')
 
-<!-- Modal -->
-<div class="modal fade" id="removeModal-{{$group->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Uwaga</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Czy na pewno chcesz usunąć wybraną grupę? {{$group->name}}
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <form method="POST" action="/manage/groups/{{$group->id}}">
-                    @csrf
-                    @method('DELETE')
-                <button class="btn btn-primary">Save changes</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 
-
+<x-modal.remove-element :idItemToRemove="$group->id" :nameItemToRemove="$group->name"/>
 
 <h3>{{$group['name']}}</h3>
 <nav class="navbar-expand">
